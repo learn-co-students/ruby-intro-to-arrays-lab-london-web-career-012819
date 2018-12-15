76f6509637764b6ab9d9ae737fe945159d95a994
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-15 20:37:51 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+def instantiate_new_array
+  arr = []
+  return arr
+end
+
+def array_with_two_elements
+  arr2 = [1,21]
+  return arr2
+end
+
+def first_element(arr3)
+  return arr3[0]
+end
+
+def third_element(arr4)
+  return arr4[2]
+end
+
+def last_element(arr5)
+  arr5[-1]
+end
+
+def first_element_with_array_methods(arr6)
+  arr6.first
+end
+
+def last_element_with_array_methods(arr7)
+  arr7.last
+end
+
+def length_of_array(arr8)
+  arr8.length
+end
+
+  
