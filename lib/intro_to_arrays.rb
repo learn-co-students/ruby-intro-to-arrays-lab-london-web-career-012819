a4782af1991e54b6d9e0a33d2869ffc0cc07fd63
Salem Qemlas <saqemlas@googlemail.com>
2019-01-19 16:24:32 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  arr = []
+  return arr
+end
+
+ def array_with_two_elements
+  arr2 = [1,21]
+  return arr2
+end
+
+ def first_element(arr3)
+  return arr3[0]
+end
+
+ def third_element(arr4)
+  return arr4[2]
+end
+
+ def last_element(arr5)
+  arr5[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.shift
+end
+
+ def last_element_with_array_methods(array)
+  return array.pop
+end
+
+ def length_of_array(array)
+  return array.count
+end
