b2157aad9f233d08760af9388ca4859f35a72118
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2019-01-25 02:25:28 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,16 @@
+def 
+instantiate_new_array
+array = []
+array
+end 
+
+def 
+  array_with_two_elements
+  array = ["Apples", "Pears"]
+  array 
+end   
+
+def first_element[array]
+  array [0]
+end  
+
